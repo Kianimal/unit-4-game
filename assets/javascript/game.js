5aad9b69806ff460f46f59b505c6e0b41dac03b7
Kianimal <spoono17@gmail.com>
2020-02-22 03:17:18 -0600

    Javascript work started. Default values set properly. Function for resets written but not enabled. Random values and score tracker working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,42 @@
-<script>
-    
-</script>
+var scoreVal = 0;
+
+var blueVal = Math.floor(Math.random()*10);
+var greenVal = Math.floor(Math.random()*10);
+var purpleVal = Math.floor(Math.random()*10);
+var redVal = Math.floor(Math.random()*10);
+
+var score = document.getElementById("userScoreNum");
+
+function targetScoreSet(){
+    var targetScore = document.getElementById("targetScoreNum");
+    targetScore.textContent = Math.floor(Math.random()*50);
+}
+
+function crystalValSet(){
+    blueVal = Math.floor(Math.random()*10);
+    greenVal = Math.floor(Math.random()*10);
+    purpleVal = Math.floor(Math.random()*10);
+    redVal = Math.floor(Math.random()*10);
+}
+
+$("#blueButton").on("click", function() {
+    scoreVal += blueVal;
+    score.textContent = scoreVal;
+});
+
+$("#greenButton").on("click", function() {
+    scoreVal += greenVal;
+    score.textContent = scoreVal;
+});
+
+$("#purpleButton").on("click", function() {
+    scoreVal += purpleVal;
+    score.textContent = scoreVal;
+});
+
+$("#redButton").on("click", function() {
+    scoreVal += redVal;
+    score.textContent = scoreVal;
+});
+
+var script = document.createElement('script');
